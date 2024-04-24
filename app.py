4723d6443c593e9e02cb2b2214cfd04672d76e3c
Oliver Day <oliver.day.swe@gmail.com>
2024-04-23 18:11:00 -0700

    feat: add 'Server' class

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def sample():
     return f'This is the {os.environ["APP"]} application.'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+@app.route("/healthcheck")
+def healthcheck():
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")

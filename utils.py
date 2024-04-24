f094dfbd669514eb45c082ad85fcf96eca420c10
Oliver Day <oliver.day.swe@gmail.com>
2024-04-23 20:16:06 -0700

    feat: add utils file

---
+++ utils.py
@@ -0,0 +1,30 @@
+import random
+import yaml
+
+from models import Server
+
+
+def load_configuration(path):
+    with open(path) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+    return config
+
+
+def transform_backends_from_config(config):
+    register = {}
+    for entry in config.get("hosts", []):
+        register.update(
+            {entry["host"]: [Server(endpoint) for endpoint in entry["servers"]]}
+        )
+    for entry in config.get("paths", []):
+        register.update(
+            {entry["path"]: [Server(endpoint) for endpoint in entry["servers"]]}
+        )
+    return register
+
+
+def get_healthy_server(host, register):
+    try:
+        return random.choice([server for server in register[host] if server.healthy])
+    except IndexError:
+        return None

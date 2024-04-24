24b6bc12f08c05e6c245ea671e9028e769b112ed
Oliver Day <oliver.day.swe@gmail.com>
2024-04-23 17:44:38 -0700

    feat: add host-based routing logic

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,7 +1,23 @@
-from flask import Flask
+import random
+
+import requests
+from flask import Flask, request
 
 loadbalancer = Flask(__name__)
 
-@loadbalancer.route('/')
+
+MANGO_BACKENDS = ["localhost:8081", "localhost:8082"]
+APPLE_BACKENDS = ["localhost:9081", "localhost:9082"]
+
+
+@loadbalancer.route("/")
 def router():
-    return 'hello'
+    host_header = request.headers["Host"]
+    if host_header == "www.mango.com":
+        response = requests.get(f"http://{random.choice(MANGO_BACKENDS)}")
+        return response.content, response.status_code
+    elif host_header == "www.apple.com":
+        response = requests.get(f"http://{random.choice(APPLE_BACKENDS)}")
+        return response.content, response.status_code
+    else:
+        return "Not Found", 404
--- test_loadbalancer.py
+++ test_loadbalancer.py
@@ -8,7 +8,17 @@ def client():
     with loadbalancer.test_client() as client:
         yield client
 
+def test_host_routing_mango(client):
+    result = client.get('/', headers={'Host': 'www.mango.com'})
+    assert b'This is the mango application.' in result.data
 
-def test_hello(client):
-    result = client.get('/')
-    assert b'hello' in result.data
+
+def test_host_routing_apple(client):
+    result = client.get('/', headers={'Host': 'www.apple.com'})
+    assert b'This is the apple application.' in result.data
+
+
+def test_host_routing_notfound(client):
+    result = client.get('/', headers={'Host': 'www.notmango.com'})
+    assert b'Not Found' in result.data
+    assert 404 == result.status_code

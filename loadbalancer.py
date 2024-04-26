6869d7c1938b70f2fcaef2df21ccd0c7653acd0c
Oliver Day <oliver.day.swe@gmail.com>
2024-04-25 19:20:40 -0700

    feat: add 'healthcheck' for host-based routing

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,29 +1,37 @@
 import random
 
 import requests
-import yaml
 from flask import Flask, request
+
+from utils import (
+    get_healthy_server,
+    healthcheck,
+    load_configuration,
+    transform_backends_from_config,
+)
+
 
 loadbalancer = Flask(__name__)
 
-
-def load_configuration(path):
-    with open(path) as config_file:
-        config = yaml.load(config_file, Loader=yaml.FullLoader)
-    return config
-
-
 config = load_configuration("loadbalancer.yaml")
+register = transform_backends_from_config(config)
 
 
 @loadbalancer.route("/")
 def router():
+    updated_register = healthcheck(register)
+    print(updated_register)
     host_header = request.headers["Host"]
     for entry in config["hosts"]:
         if host_header == entry["host"]:
-            response = requests.get(f'http://{random.choice(entry["servers"])}')
+            healthy_server = get_healthy_server(entry["host"], updated_register)
+            if not healthy_server:
+                return "No backend servers available.", 503
+            response = requests.get(f"http://{healthy_server.endpoint}")
             return response.content, response.status_code
-    return "Not Found", 404
+
+    updated_register = healthcheck(register)
+    print(updated_register)
 
 
 @loadbalancer.route("/<path>")

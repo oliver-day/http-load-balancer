c0a4184d3b072cf429365b1c6706e1ef21cd8798
Oliver Day <oliver.day.swe@gmail.com>
2024-04-25 19:30:50 -0700

    feat: add 'healthcheck' for path-based routing

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,5 +1,3 @@
-import random
-
 import requests
 from flask import Flask, request
 
@@ -18,9 +16,9 @@ register = transform_backends_from_config(config)
 
 
 @loadbalancer.route("/")
-def router():
+@loadbalancer.route("/<path>")
+def router(path="/"):
     updated_register = healthcheck(register)
-    print(updated_register)
     host_header = request.headers["Host"]
     for entry in config["hosts"]:
         if host_header == entry["host"]:
@@ -29,15 +27,11 @@ def router():
                 return "No backend servers available.", 503
             response = requests.get(f"http://{healthy_server.endpoint}")
             return response.content, response.status_code
-
-    updated_register = healthcheck(register)
-    print(updated_register)
-
-
-@loadbalancer.route("/<path>")
-def path_router(path):
     for entry in config["paths"]:
         if ("/" + path) == entry["path"]:
-            response = requests.get(f'http://{random.choice(entry["servers"])}')
+            healthy_server = get_healthy_server(entry["path"], register)
+            if not healthy_server:
+                return "No backend servers available.", 503
+            response = requests.get(f"http://{healthy_server.endpoint}")
             return response.content, response.status_code
     return "Not Found", 404
--- test_loadbalancer.py
+++ test_loadbalancer.py
@@ -9,7 +9,6 @@ def client():
         yield client
 
 
-# Host-based routing
 def test_host_routing_mango(client):
     result = client.get("/", headers={"Host": "www.mango.com"})
     assert b"This is the mango application." in result.data
@@ -20,13 +19,17 @@ def test_host_routing_apple(client):
     assert b"This is the apple application." in result.data
 
 
+def test_host_routing_orange(client):
+    result = client.get("/", headers={"Host": "www.orange.com"})
+    assert b"No backend servers available." in result.data
+
+
 def test_host_routing_notfound(client):
     result = client.get("/", headers={"Host": "www.notmango.com"})
     assert b"Not Found" in result.data
     assert 404 == result.status_code
 
 
-# Path-based routing
 def test_path_routing_mango(client):
     result = client.get("/mango")
     assert b"This is the mango application." in result.data
@@ -35,6 +38,11 @@ def test_path_routing_mango(client):
 def test_path_routing_apple(client):
     result = client.get("/apple")
     assert b"This is the apple application." in result.data
+
+
+def test_path_routing_orange(client):
+    result = client.get("/orange")
+    assert b"No backend servers available." in result.data
 
 
 def test_path_routing_notfound(client):
